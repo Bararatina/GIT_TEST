e47751061deed4a5838be7df211b70c01594fc27
Bararatina <lee9494@mail.ru>
2023-03-28 15:17:09 +0700

    delete unnecessary argument

--- Transport.py
+++ Transport.py
@@ -31,8 +31,6 @@ class Airplane(Transport):
         # связываем с родителем и наследуем max_speed и year
         self.type_of_airplane = type_of_airplane
         self.flight_altitude = flight_altitude
-        self.height = flight_altitude
-        # add new parameter equals to flight altitude
 
     def get_info(self):
         super().get_info()
@@ -41,8 +39,7 @@ class Airplane(Transport):
 
     def change_height(self, height_diff):  # add method for changing height
         '''Changing flight altitude'''
-        self.height += height_diff
-        self.flight_altitude = self.height
+        self.flight_altitude += height_diff
         super().get_info()
         print('Type of airplane:', self.type_of_airplane,
               'Flight altitude:', self.flight_altitude)
@@ -64,9 +61,10 @@ class car(Transport):
 
 airplane1 = Airplane('Airplane', 'Passenger', 10000, 450, 2005)
 airplane2 = Airplane('Airplane', 'Passenger', 10000, 450, 2005)
-print(airplane1)
+
+# print(airplane1)
 # airplane.get_info()     # delete if we want print only new height
 # car = car('Car', 'BMW', 'four-wheel drive', 250, 2020)
 # car.get_info()
-# airplane.change_height(-3000)    # difference between altitudes
+# airplane1.change_height(-3000)    # difference between altitudes
 # print(airplane.change_height.__doc__)   # print docstring
